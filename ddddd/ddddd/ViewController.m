c96a95dfa519b27cc3aeae6feca50fa7308e6a72
殷云鹏 <1145950939@qq.com>
2017-06-21 17:05:13 +0800

    label添加

--- ddddd/ddddd/ViewController.m
+++ ddddd/ddddd/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UILabel *label = [UILabel new];
+    [self.view addSubview:label];
 }
 
 
